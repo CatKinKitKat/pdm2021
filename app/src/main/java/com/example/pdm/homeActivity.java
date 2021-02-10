5ffd881a2e804018a5f681bb35ec083f1d08525e
PLOGA <pfbtomas@gmail.com>
2021-02-10 13:39:55 +0000

    hgiofsda

--- app/src/main/java/com/example/pdm/homeActivity.java
+++ app/src/main/java/com/example/pdm/homeActivity.java
@@ -10,5 +10,6 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //aahjnpfosejpasf
     }
 }
