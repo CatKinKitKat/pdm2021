0fd3d1171db22218fb3af4bf87a41fb8f8a6aa43
PLOGA <pfbtomas@gmail.com>
2021-02-11 02:44:39 +0000

    updates: Firebase linkada Sistema de login e registro funcional Começo da implementação da página do utilizador Sistema de logout Sistema de login permanente caso seja o mesmo utilizador Sistema de verificação de email funcional Alteração na toolbar da main activity

---
+++ app/src/main/java/com/ipbeja/easymed/profileActivity.java
@@ -0,0 +1,88 @@
+package com.ipbeja.easymed;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class profileActivity extends AppCompatActivity {
+
+    TextView verifyMsg;
+    Button verifyEmailBtn;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+        auth = FirebaseAuth.getInstance();
+        verifyMsg = findViewById(R.id.verifyEmailMsg);
+        verifyEmailBtn = findViewById(R.id.verifyEmailBtn);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if(!auth.getCurrentUser().isEmailVerified()){
+
+            verifyEmailBtn.setVisibility(View.VISIBLE);
+            verifyMsg.setVisibility(View.VISIBLE);
+
+        }
+
+        verifyEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send verification email
+                auth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(profileActivity.this,"Verification Email Sent", Toast.LENGTH_SHORT).show();
+                        verifyEmailBtn.setVisibility(View.GONE);
+                        verifyMsg.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(),loginActivity.class));
+            finish();
+
+        }else if (id == android.R.id.home) {
+
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
